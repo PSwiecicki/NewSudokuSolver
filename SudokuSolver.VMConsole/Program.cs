409f78b0980e4571b22c081280dde04b322a519c
Patryk Święickci <patswiecicki@gmail.com>
2021-04-26 22:56:28 +0200

    Add option to insert data to sudoku solver from console

--- SudokuSolver.VMConsole/Program.cs
+++ SudokuSolver.VMConsole/Program.cs
@@ -8,7 +8,35 @@ namespace SudokuSolver.VMConsole
     {
         static void Main(string[] args)
         {
+            Sudoku sudoku = new Sudoku();
+
+            InsertData(sudoku);
+
+            sudoku.Solve();
+
+            Console.WriteLine(sudoku.ToString());
             
+        }
+
+        static void InsertData(Sudoku sudoku)
+        {
+            int[,] dataForSudoku = new int[9, 9];
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Podaj wartość pola lub 0 jeżeli nie nie ma informacji");
+                    Console.WriteLine($"Pole ({i}, {j}): ");
+                    string response = Console.ReadLine();
+                    if(!int.TryParse(response, out int result))
+                    {
+                        result = 0;
+                    }
+                    dataForSudoku[i, j] = result;
+                }
+            }
+            sudoku.InsertData(dataForSudoku);
         }
     }
 }
