99ae85e2a8d28ce93c7a8949502d2a8168e6088e
Patryk Święickci <patswiecicki@gmail.com>
2021-05-17 23:59:15 +0200

    Add function to color conflicted fields

--- SudokuSolver.VMWPF/MainWindow.xaml.cs
+++ SudokuSolver.VMWPF/MainWindow.xaml.cs
@@ -38,25 +38,9 @@ namespace SudokuSolver.VMWPF
                                                   { Field54, Field55, Field56, Field57, Field58, Field59, Field60, Field61, Field62 },
                                                   { Field63, Field64, Field65, Field66, Field67, Field68, Field69, Field70, Field71 },
                                                   { Field72, Field73, Field74, Field75, Field76, Field77, Field78, Field79, Field80 }};
-            dataTable = new int[9, 9];
-            for (int i = 0; i < 9; i++)
-            {
-                for(int j = 0; j < 9; j++)
-                {
-                    if(FieldsTextboxes[i, j].Text != "")
-                    {
-                        int.TryParse(FieldsTextboxes[i, j].Text, out dataTable[i, j]);
-                    }
-                    else
-                    {
-                        dataTable[i, j] = 0;
-                    }
-                }
-            }
         }
 
         TextBox[,] FieldsTextboxes;
-        int[,] dataTable; 
 
 
         private void FieldPreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -64,7 +48,51 @@ namespace SudokuSolver.VMWPF
             Regex regex = new Regex("[^1-9]+");
             e.Handled = regex.IsMatch(e.Text);
             var field = sender as TextBox;
+            var indexes = GetTexboxIndexes(field);
+            var conflictedFields = GetConflictFields(indexes[0], indexes[1]);
+            if(conflictedFields.Count > 0)
+            {
+                foreach(var conflictedField in conflictedFields)
+                {
+                    conflictedField.Background = Brushes.LightCoral;
+                }
+            }
+        }
 
+        private List<TextBox> GetConflictFields(int row, int column)
+        {
+
+            List<TextBox> textBoxes = new List<TextBox>();
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == row)
+                    continue;
+                if (FieldsTextboxes[i, column].Text == FieldsTextboxes[row, column].Text)
+                    textBoxes.Add(FieldsTextboxes[i, column]);
+            }
+            for (int j = 0; j < 9; j++)
+            {
+                if (j == column)
+                    continue;
+                if (FieldsTextboxes[row, j].Text == FieldsTextboxes[row, column].Text)
+                    textBoxes.Add(FieldsTextboxes[row, j]);
+            }
+            int square = row / 3 * 3 + column / 3;
+            int squareFirstFieldIndexI = square / 3 * 3;
+            int squareFirstFieldIndexJ = (square % 3) * 3;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int checkingI = squareFirstFieldIndexI + i;
+                    int checkingJ = squareFirstFieldIndexJ + j;
+                    if (checkingI == row && checkingJ == column)
+                        continue;
+                    if (FieldsTextboxes[checkingI, checkingJ].Text == FieldsTextboxes[row, column].Text)
+                        textBoxes.Add(FieldsTextboxes[checkingI, checkingJ]);
+                }
+            }
+            return textBoxes;   
         }
 
         private int[] GetTexboxIndexes(TextBox textBox)
