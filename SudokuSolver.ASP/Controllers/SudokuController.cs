c6d31d0725164158c4d4a20e28ca0863a7b890f1
Patryk Święickci <patswiecicki@gmail.com>
2021-09-18 14:12:31 +0200

    Add ASP server with API to solve my sudoku

---
+++ SudokuSolver.ASP/Controllers/SudokuController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SudokuSolver.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.ASP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SudokuController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<List<int>>> Solve() =>
+            Services.SudokuService.Solve();
+
+        [HttpGet("{data}")]
+        public ActionResult<bool> Get(string data)
+        {
+            if (data.Length != 81) return false;
+            var result = true;
+            var dataTable = new int[9, 9];
+
+            for(int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    result &= int.TryParse("" + data[i * 9 + j], out dataTable[i, j]);
+                }
+            }
+
+            result &= Services.SudokuService.InsertaData(dataTable);
+
+            return result;
+        }
+    }
+}
---
+++ SudokuSolver.ASP/Services/SudokuService.cs
@@ -0,0 +1,52 @@
+﻿using SudokuSolver.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.ASP.Services
+{
+    public static class SudokuService
+    {
+        static Sudoku sudoku { get; }
+
+        static SudokuService()
+        {
+            //int[,] table = new int[,]
+            //{
+            //    { 4, 0, 5, 7, 0, 0, 0, 0, 0 },
+            //    { 9, 2, 0, 0, 0, 0, 0, 0, 0 },
+            //    { 0, 0, 0, 0, 0, 0, 1, 5, 8 },
+            //    { 0, 0, 0, 0, 0, 0, 0, 6, 9 },
+            //    { 0, 8, 0, 0, 0, 6, 7, 0, 0 },
+            //    { 0, 9, 0, 0, 0, 0, 0, 0, 1 },
+            //    { 6, 0, 0, 0, 9, 0, 0, 0, 3 },
+            //    { 0, 0, 0, 0, 0, 7, 6, 0, 0 },
+            //    { 5, 0, 0, 1, 0, 0, 0, 0, 2 }
+            //};
+
+            sudoku = new Sudoku();
+            //sudoku.InsertData(table);
+        }
+
+        public static List<List<int>> Solve()
+        {
+            sudoku.Solve();
+            List<List<int>> result = new List<List<int>>();
+            foreach(var row in sudoku.Rows)
+            {
+                result.Add(new List<int>());
+                foreach(var field in row.Fields)
+                {
+                    result[result.Count - 1].Add(field.Value);
+                }
+            }
+            return result;
+        }
+        public static bool InsertaData(int[,] data)
+        {
+            sudoku.InsertData(data);
+            return sudoku.IsValid;
+        }
+    }
+}
